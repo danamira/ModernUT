d879ec637f36e5d5383986adb1c4bdf4b8cffb40
= <='danadev94@gmail.com>
2021-10-08 17:49:47 +0330

    Initial commit

---
+++ content.js
@@ -0,0 +1,20 @@
+	$('.logo img').attr('src','https://i.postimg.cc/5y57gwrV/logo.png')
+	$('.logo img').width(323)
+	$('.event,.event-item span .icon')
+	$('#site-footer').hide()
+	$('#site-footer').after(`<footer class="dutfooter">
+		<div class="right">
+		<p>
+		<i class="fa fa-copyright"></i>
+		سامانه آموزش الکترونیکی دانشگاه تهران. 
+		</p>
+		</div>
+		<div class="left">
+		<p>
+		پوسته ModernUT و  کار های دیگر را
+		<a href="https://twitter.com/danmirafzal"><i class="fa fa-twitter"></i>توییتر</i></a>
+		دنبال کنید.
+		</p>
+		</div>
+		</footer>`)
+	$('.form-in-iframe').css('border-radius','4px')
