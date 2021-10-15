41a0c402bf0ef818798648a872d7dc51440da0e1
= <='danadev94@gmail.com>
2021-10-16 00:51:21 +0330

    Minor changes

--- content.js
+++ content.js
@@ -4,21 +4,20 @@
 	$('.event,.event-item span .icon')
 	$('#site-footer').hide()
 	$('#site-footer').after(`<footer class="dutfooter">
-		<div class="right">
-		<p>
-		<i class="fa fa-copyright"></i>
-		سامانه آموزش الکترونیکی دانشگاه تهران. 
-		</p>
-		</div>
-		<div class="left">
-		<p>
-		پوسته ModernUT و  کار های دیگر را
-		<a href="https://twitter.com/danmirafzal"><i class="fa fa-twitter"></i>توییتر</i></a>
-		دنبال کنید.
-		</p>
-		</div>
-		</footer>`)
+	<div class="right">
+	<p>
+	<i class="fa fa-copyright"></i>
+	سامانه آموزش الکترونیکی دانشگاه تهران. 
+	</p>
+	</div>
+	<div class="left">
+	<p>
+	پوسته ModernUT و  کار های دیگر را
+	<a href="https://twitter.com/danmirafzal"><i class="fa fa-twitter"></i>توییتر</i></a>
+	دنبال کنید.
+	</p>
+	</div>
+	</footer>`)
 	$('.form-in-iframe').attr('style','border-radius:4px !important')
 	$('#login-form .btn').attr("value",'ورود به حساب کاربری')
 	$('.form-in-iframe .logo').attr('src',chrome.runtime.getURL("Assets/AuthLogo.png"))
-	
